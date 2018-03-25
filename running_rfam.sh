aef683624a50cb6a08b61d1a8ff6262b9e4bc2f2
Saldana_Farias_Beatriz_E_bsaldana3 <beatrizsaldana@gatech.edu>
2018-03-25 19:37:37 -0400

    fixing directory sctructure

--- running_rfam.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for file in *.fasta
-do
-	OUTPUT="$(esl-seqstat $file | grep "Total" | grep -o [0-9]*)"
-    Z="$(echo $((OUTPUT * 2 / 1000000)))"
-    cmscan -Z $Z --cut_ga --rfam --nohmmonly --cpu 6 --tblout /projects/data/gene_prediction_team2/predicted_genes/rfam_results/$file.tblout --fmt 2 --clanin /projects/data/gene_prediction_team2/tools/ncRNAs/Rfam_database/Rfam.clanin /projects/data/gene_prediction_team2/tools/ncRNAs/Rfam_database/Rfam.cm $file > /projects/data/gene_prediction_team2/predicted_genes/rfam_results/$file.cmscan
-    grep -v " = " /projects/data/gene_prediction_team2/predicted_genes/rfam_results/$file.tblout > /projects/data/gene_prediction_team2/predicted_genes/rfam_results/$file.deoverlapped.tblout
-done
--- scripts/rfam2gff.sh
+++ scripts/rfam2gff.sh
@@ -8,8 +8,8 @@ do
 		readline=$line
 		if [[ $readline =~ ^[0-9]+ ]]
 		then
-			echo $readline | awk '$12 == "+" {printf "%s\tInfernal-1.1.2/Rfam\t%s\t%d\t%d\t%s\t%s\t.\tTarget_Name:%s;Model:%s;Target/RF_ACC:%s;Clan_Name:%s;GC:%s\n" ,$4,$2,$3,$10,$11,$18,$12,$2,$7,$3,$6,$15}' >> $file.gff
-			echo $readline | awk '$12 == "-" {printf "%s\tInfernal-1.1.2/Rfam\t%s\t%d\t%d\t%s\t%s\t.\tTarget_Name:%s;Model:%s;Target/RF_ACC:%s;Clan_Name:%s;GC:%s\n" ,$4,$2,$3,$11,$10,$18,$12,$2,$7,$3,$6,$15}' >> $file.gff
+			echo $readline | awk '$12 == "+" {printf "%s\tInfernal-1.1.2/Rfam\t%s\t%d\t%d\t%s\t%s\t.\tTarget_Name:%s;Model:%s;Target/RF_ACC:%s;Clan_Name:%s;GC:%s\n" ,$4,$2,$10,$11,$18,$12,$2,$7,$3,$6,$15}' >> $file.gff
+			echo $readline | awk '$12 == "-" {printf "%s\tInfernal-1.1.2/Rfam\t%s\t%d\t%d\t%s\t%s\t.\tTarget_Name:%s;Model:%s;Target/RF_ACC:%s;Clan_Name:%s;GC:%s\n" ,$4,$2,$11,$10,$18,$12,$2,$7,$3,$6,$15}' >> $file.gff
 		fi
 	done < $file
 done
