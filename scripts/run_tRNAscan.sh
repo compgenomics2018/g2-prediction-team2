052b899680ae8742739958af5e93a635d5d00c9d
Siu Lung Ng (Michael) <sng32@gatech.edu>
2018-03-26 21:51:45 -0400

    Run tRNAscan and output gff files

--- scripts/run_tRNAscan.sh
+++ scripts/run_tRNAscan.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
-while getopts "g:o:" agrs
+while getopts "g:p:o:" agrs
 do
     case $agrs in
 	g)    	      	      	      	      	      	      	      	# Path to list of genomes                                                                                        
         if [ -f $OPTARG ]
 		then
-            genomepath=$OPTARG;
+            genome=$OPTARG;
         else
             echo "[-g] $OPTARG cannot be found in the directory";   # Check for existence of files  
+            exit;                                                                                 
+		fi;;
+	p)
+		if [ -f $OPTARG ]
+		then
+            path=$OPTARG;
+        else
+            echo "[-p] $OPTARG cannot be found in the directory";   # Check for existence of files  
             exit;                                                                                 
 		fi;;
 	o)
@@ -24,11 +32,13 @@ done
 
 while read -r line
 do
-	tRNAscan-SE -B -o "$outpath/$line.out" "$genomepath/$line";
-done
+	tRNAscan-SE -B -o "$outpath/$line.out" "$path/$line";
+done < $genome
 
 for file in "$outpath/*.out"
 do
     i=$(echo $file | awk -F  "." '{print $1}')
-    perl tRNAscan_SE_to_GFF_edited.pl --input="$outpath/$file" > $i.gff
+    perl scripts/tRNAscan_SE_to_GFF_edited.pl --input="$outpath/$file" > $i.gff
 done
+
+rm *.out
