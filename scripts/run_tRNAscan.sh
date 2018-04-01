30270781e62f1fdde81f487eda157c6bec33aae6
Siu Lung Ng (Michael) <sng32@gatech.edu>
2018-04-01 01:30:10 -0400

    fixed bugs in run_tRNAscan.sh

--- scripts/run_tRNAscan.sh
+++ scripts/run_tRNAscan.sh
@@ -12,33 +12,19 @@ do
             exit;                                                                                 
 		fi;;
 	p)
-		if [ -f $OPTARG ]
-		then
-            path=$OPTARG;
-        else
-            echo "[-p] $OPTARG cannot be found in the directory";   # Check for existence of files  
-            exit;                                                                                 
-		fi;;
+        path=$OPTARG;;
 	o)
-		if [ -f $OPTARG ]
-		then
-            outpath=$OPTARG;
-        else
-            echo "[-o] $OPTARG cannot be found in the directory";   # Check for existence of files
-            exit;                                                                                   
-		fi;;
+        outpath=$OPTARG;;
 	esac
 done
 
 while read -r line
 do
-	tRNAscan-SE -B -o "$outpath/$line.out" "$path/$line";
+	tRNAscan-SE -B -o "$outpath$line.out" "$path$line";
 done < $genome
 
-for file in "$outpath/*.out"
+for file in $outpath*.out
 do
-    i=$(echo $file | awk -F  "." '{print $1}')
-    perl scripts/tRNAscan_SE_to_GFF_edited.pl --input="$outpath/$file" > $i.gff
+	i=$(echo $file | awk -F "/" '{print $4}' |awk -F  "." '{print $1}')
+    perl scripts/tRNAscan_SE_to_GFF_edited.pl --input="$file" > $outpath$i.gff
 done
-
-rm *.out
