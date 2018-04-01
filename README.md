df2aa47aa11a620b3019936727fe68a8ae522f7e
Saldana_Farias_Beatriz_E_bsaldana3 <beatrizsaldana@gatech.edu>
2018-03-31 21:42:44 -0400

    edited the README, fixed some pipeline issues, added the gff cleaning script to pipeline, and added -f, -g, and -h flags to pipeline

--- README.md
+++ README.md
@@ -1,37 +1,64 @@
-# Gene Prediction
+Team 2, Group 2 - Gene Prediction
 
-This repository contains a set of gene prediction scripts for Team II's Gene Prediction Group. 
+This repository contains a pipeline to predict genes from assembled genomes in fasta format. 
 
+##### Tools used by this pipeline:
+      Prodigal, GeneMark-S (or GeneMark-HMM when '-f' flag is used), Rfam, Aragorn, and tRNAscan
+
+
+##### HOW TO RUN
+    
+    1. Put all of the assembled genomes (in fasta format) into a directory
+    2. Run this command:  ./genePredictionPipelineT2G2.sh -p /path/to/assemblies [-f, -h, -t, and -v are optional]
+
+    Usage: [-p /path/to/assemblies], [-f to create fasta and gff files], [-g for faster predictions, will run GeneMarkHMM instead of GeneMarkS], [-t to keep temp files and directories], [-v for verbose mode], [-h for help/usage]
+
+
+##### Input and Outputs
+      input: path to assemblies
+      temp: results of each individual tool and step (this directory will be removed unless otherwise stated)
+      output: one gff file per sample containing all predicted genes (union of all 5 tools)
+      	      if '-f' is used, fasta files will also be created
+
+#####
 For general info, see the [project's wiki page](http://www.compgenomics2018.biosci.gatech.edu/index.php?title=Team_II_Gene_Prediction_Group).
+#####
 
-## File Structure and Usage
+
+##### Directory structure:
 
 The repository is organized as follows:
 
-    .
+    
     ├── final_pipeline.sh       # Final gene prediction pipeline
-    ├── gene prediction         # A set of scripts for the actual gene prediction
-    │   ├── ab-initio           # ab-initio gene prediction scripts
-    │   ├── reference           # Homology gene prediction scripts
-    ├── validation              # Blast
-    └
+    ├── README.md         	# Document you are currently reading, describes the pipeline
+    ├── scripts/		# Directory containing all of the scripts necessary to run the pipeline 
+    ├──	results			# Directory containing the final results (will be created when pipeline is run)
+    	├── gff			# If '-f' is used, results in gff format will be stored here
+        ├── fasta		# If '-f' is used, gff to fasta conversion results will be placed here
+    ├── temp			# Directory containing temporary files (will be deleted unless '-t' is used)
 
-To predict a gene, run
+The temp/ directory is organized as follows:
 
-    ./final_pipeline 
+├── temp/
+    ├── genePrediction
+    	├── prodigalResults	# Directory containing results from Prodigal
+	├── geneMarkResults	# Directory containing results from GeneMark-S (or GeneMark-HMM if '-g' is used)
+	├── rfamResults		# Directory containing results from Rfam
+	    ├── other		# Directory containing files created when Rfam is run
+	├── aragnoResults	# Directory containing results from Aragorn
+	├──tRNAscanResults	# Directory containing results from tRNAscan
+    ├── merging			# Directory containing all of the merged results
+    	├── abinitioMerge	# Directory containing the merged results of Prodigal and GeneMark
+	├── ncRNAMerge		# Directory containing the merged results of Rfam, Aragorn, and tRNAscan
+	├── allFiles		# Directory containing the merged gff files (before cleaning)
+    ├── results			# Directory containing the final results
+    	├── gff			# If '-f' is used, results in gff format will be stored here
+	├── fasta		# If '-f' is used, gff to fasta conversion results will be placed here
 
-To display additional options, run
 
-    ./final_pipeline -h
-
-The remaining folders contain individual scripts which are not necessarily included in the final pipeline. Some of these scripts **might only work on** `biogenome2018b.biology.gatech.edu` server since they rely on specific files present on this server.
-
-## Installation & Dependencies
+##### Installation & Dependencies
 
 No installation is needed, only thing required is to clone the repository and run the `final_pipeline.sh` script.
 
 However, several dependencies are assumed in the path:
-
-
-	
-A description of each of these tools is available in the [project's wiki](http://www.compgenomics2018.biosci.gatech.edu/Team_II_Genome_Assembly_Group).
--- genePredictionPipelineT2G2.sh
+++ genePredictionPipelineT2G2.sh
@@ -6,43 +6,49 @@
 ##### HOW TO RUN
 ### put all of the assembled genomes into a directory
 ### Run this command:  ./genePredictionPipelineT2G2.sh -p /path/to/assemblies [-t and -v are optional]
-### Usage: [-p /path/to/assemblies], [-f for faster predictions, will run GeneMarkHMM instead of GeneMarkS, will not run Rfam],
-###[-t to keep temp filed and directories], [-v for verbose mode]
+### Usage: [-p /path/to/assemblies], [-h for faster predictions, will run GeneMarkHMM instead of GeneMarkS],
+### [-g for faster predictions, will run GeneMarkHMM instead of GeneMarkS],
+### [-t to keep temp filed and directories], and [-v for verbose mode]
 
 ##### Input and Outputs
 ### input: path to assemblies
 ### temp: results from each individual tool (this directory will be removed unless otherwise stated)
 ### output: one gff file per sample containing all predicted genes (union of all 5 tools)
+###         if '-f' is used, fasta files will also be created
 
 ###################################################################### Command line Arguments
 t=0
 v=0
+g=0
 f=0
 
 while getopts ":p:ftv" opt; do
   case ${opt} in
-    p ) path=$OPTARG
+      p ) path=$OPTARG
 	  ;;
-      f )f=1
+      g )g=1
 	  ;;
       t )t=1
 	  ;;
-    v ) v=1
+      f )f=1
 	  ;;
-    \? ) echo "Usage: [-p path/to/assembledGenomes], [[-f for faster predictions, will run GeneMarkHMM instead of GeneMarkS, will not run Rfam], [-t to keep temp files and directories], [-v for verbose mode]"
-      ;;
+      v )v=1
+	  ;;
+      h ) printf "Usage: [-p path/to/assembledGenomes], [-h for faster predictions, will run GeneMarkHMM instead of GeneMarkS], [-f to create both gff and fasta files], [-t to keep temp files and directories], and [-v for verbose mode]\\n"
+	  ;;
+    \? ) printf "Usage: [-p path/to/assembledGenomes], [-h for faster predictions, will run GeneMarkHMM instead of GeneMarkS], [-f to create both gff and fasta files], [-t to keep temp files and directories], and [-v for verbose mode]\\n"
+	  ;;
   esac
 done
 
 ###################################################################### Create directory structure
 
 #warning statement when fast version of script is run
-if [ $f == 1 ]; then
+if [ $g == 1 ]; then
     printf "****************************************************************************\\n\\n"
     printf "WARNING!\\n"
     printf "You are running the fast version of the script, this might affect the quality of your results.\\n"
     printf "GeneMark-S will not be run. GeneMark-HMM will be run instead.\\n"
-    printf "Rfam will not be run. Results might contain less sRNA gene predictions.\\n\\n"
     printf "****************************************************************************\\n\\n"
 fi
 
@@ -53,18 +59,22 @@ fi
 
 mkdir temp
 mkdir temp/genePrediction
-mkdir temp/merging
 mkdir temp/genePrediction/prodigalResults
 mkdir temp/genePrediction/geneMarkResults
 mkdir temp/genePrediction/rfamResults
 mkdir temp/genePrediction/rfamResults/other
-mkdir temp/genePrediction/aragonResults
+mkdir temp/genePrediction/aragornResults
 mkdir temp/genePrediction/tRNAscanResults
+mkdir temp/merging
 mkdir temp/merging/abinitioMerge
-mkdir temp/merging/abinitioMerge/clean
 mkdir temp/merging/ncRNAMerge
-mkdir temp/merging/ncRNAMerge/clean
+mkdir temp/merging/allFiles
 mkdir results
+
+if [ $f == 1 ]; then
+    mkdir results/gff
+    mkdir results/fasta
+fi
 
 if [ $v == 1 ]; then
     printf "Done!\\n"
@@ -101,7 +111,7 @@ fi
 
 ##### GeneMark (if -f flag is used, GeneMarkHMM will be run, otherwise GeneMarkS will run)
 
-if [ $f == 0 ]; then
+if [ $g == 0 ]; then
     if [ $v == 1 ]; then
 	printf "Predicting genes using GeneMark-S...\\n"
 	fi
@@ -114,7 +124,7 @@ if [ $f == 0 ]; then
     if [ $v == 1 ]; then
 	printf "Done!\\n"
 	fi
-elif [ $f == 1 ]; then
+elif [ $g == 1 ]; then
     if [ $v == 1 ]; then
 	printf "Predicting genes using GeneMark-HMM...\\n"
 	fi
@@ -135,25 +145,27 @@ if [ $v == 1 ]; then
     printf "Starting to predict genes on non-Coding RNA. Almost there!!\\n"
 fi
 
-if [ $f == 0 ]; then
-    if [ $v == 1 ]; then
-	printf "Predicting genes on non-coding RNA using Rfam...\\n"
-	fi
-
-    ./run_rfam.sh -g temp/fileList.txt -c scripts/other/Rfam.clanin -m scripts/other/Rfam.cm -t temp/genePrediction/rfamResults/ -o temp/genePrediction/other/rfamResults/other/ -p $path
-
-    if [ $v == 1 ]; then
-	printf "Done!\\n"
-	fi
+if [ $v == 1 ]; then
+    printf "Predicting genes on non-coding RNA using Rfam...\\n"
 fi
 
-##### Aragon
+printf "Warning! The database files are zipped when downloaded from github, make sure they are unzipped before running this pipeline. "
+
+printf "Rfam might not run correctly.\\n"
+
+./scripts/run_rfam.sh -g temp/fileList.txt -c scripts/other/Rfam.clanin -m scripts/other/Rfam.cm -t temp/genePrediction/rfamResults/ -o temp/genePrediction/rfamResults/other/ -p $path
 
 if [ $v == 1 ]; then
-    printf "Predicting genes on non-coding RNA using Aragon...\\n"
+    printf "Done!\\n"
 fi
 
-./run_arag.sh -g temp/fileList.txt -p $path -o temp/genePrediction/aragonResults/
+##### aragorn
+
+if [ $v == 1 ]; then
+    printf "Predicting genes on non-coding RNA using aragorn...\\n"
+fi
+
+./scripts/run_arag.sh -g temp/fileList.txt -p $path -o temp/genePrediction/aragornResults/
 
 if [ $v == 1 ]; then
     printf "Done!\\n"
@@ -165,7 +177,7 @@ if [ $v == 1 ]; then
     printf "Predicting genes on non-coding RNA using tRNAscan...\\n"
 fi
 
-./run_tRNAscan.sh -g temp/fileList.txt -p $path -o temp/genePrediction/tRNAscanResults/
+./scripts/run_tRNAscan.sh -g temp/fileList.txt -p $path -o temp/genePrediction/tRNAscanResults/
 
 if [ $v == 1 ]; then
     printf "Done!\\n"
@@ -183,21 +195,23 @@ fi
 
 ##### Merge Prodigal + GeneMarkS Results
 
-if [ $f == 0 ]; then
-    if [ $v == 1 ]; then
-	printf "Merging Prodigal and GeneMarkS results...\\n"
-	fi
 
-    python ab_initio_merged.py -a temp/fileList.txt -i1 temp/genePrediction/geneMarkResults -i2 temp/genePrediction/prodigalResults -o temp/merging/abinitioMerge/ -f gff
-
-    ls temp/merging/abinitioMerge/ > temp/abinitioMerge_list.txt
-
-    perl scripts/clean_gff.pl temp/abinitioMerge_list.txt temp/merging/abinitioMerge/ temp/merging/abinitioMerge/clean/
-
-    if [ $v == 1 ]; then
-	printf "Done!\\n"
-	fi
+if [ $v == 1 ]; then
+    printf "Merging Prodigal and GeneMarkS results...\\n"
 fi
+
+python scripts/ab_initio_merged.py -a temp/fileList.txt -i1 temp/genePrediction/geneMarkResults/ -i2 temp/genePrediction/prodigalResults/ -o temp/merging/abinitioMerge/ -f gff
+
+ls temp/merging/abinitioMerge/ > temp/abinitioMerge_list.txt
+
+mkdir temp/merging/abinitioMerge/clean
+
+perl scripts/clean_gff.pl temp/abinitioMerge_list.txt temp/merging/abinitioMerge/ temp/merging/abinitioMerge/clean/
+
+if [ $v == 1 ]; then
+    printf "Done!\\n"
+fi
+
 
 ##### Merge ncRNA Results
 
@@ -205,9 +219,11 @@ if [ $v == 1 ]; then
     printf "Merging non-coding RNA results...\\n"
 fi
 
-#script to merge all ncRNA results > write too /merging/ncRNAMerge
+python scripts/ncRNA_merged.py -a temp/fileList.txt -i1 temp/genePrediction/rfamResults/ -i2 temp/genePrediction/tRNAscanResults/ -i3 temp/genePrediction/aragornResults/ -o temp/merging/ncRNAMerge/ -f gff
 
 ls temp/merging/ncRNAMerge/ > temp/ncRNAMerge_list.txt
+
+mkdir temp/merging/ncRNAMerge/clean
 
 perl scripts/clean_gff.pl temp/ncRNAMerge_list.txt temp/merging/ncRNAMerge/ temp/merging/ncRNAMerge/clean/
 
@@ -221,7 +237,7 @@ if [ $v == 1 ]; then
     printf "Merging all results...\\n"
 fi
 
-# script to cat results
+./scripts/cat_gff.sh
 
 if [ $v == 1 ]; then
     printf "Done!\\n"
@@ -231,12 +247,49 @@ if [ $v == 1 ]; then
     printf "Finished merging everything!!\\n"
 fi
 
-###################################################################### Clean everything
+ls temp/merging/allFiles/ > temp/mergedFilesList.txt
+
+###################################################################### Clean gff files
+
+if [ $v == 1 ]; then
+    printf "Cleaning the files...\\n"
+fi
+
+if [ $f == 0 ]; then
+perl scripts/clean_gff.pl temp/mergedFilesList.txt temp/merging/allFiles/ results/
+fi
+
+if [ $f == 1 ]; then
+perl scripts/clean_gff.pl temp/mergedFilesList.txt temp/merging/allFiles/ results/gff/
+fi
+
+if [ $v == 1 ]; then
+    printf "Done!\\n"
+fi
+
+###################################################################### Final Steps
+
+##### Create fasta files if '-f' is used                                                                           
+
+if [ $f == 1 ]; then
+
+    if [ $v == 1 ]; then
+        printf "Converting GFF files to Fasta format...\\n"
+    fi
+
+    # add script to convert gff to fasta                                                                                
+
+    if [ $v == 1 ]; then
+        printf "Done!\\n"
+    fi
+fi
+
+##### Final Statements
 
 if [ $t == 0 ]; then
     rm -r temp
 elif [ $t == 1 ]; then
-    echo "Temp directory will not be deleted.\\n"
+    printf "Temp directory will not be deleted.\\n"
 fi
 
 if [ $v == 1 ]; then
